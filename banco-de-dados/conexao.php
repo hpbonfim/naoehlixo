1a4a2bf2e0a20ecf9b9883833dee5e17efdecf5c
hpbonfim <hp_bonfim@hotmail.com>
2020-10-27 21:22:28 -0300

    adicionando mysql

--- banco-de-dados/conexao.php
+++ banco-de-dados/conexao.php
@@ -1,4 +1,19 @@
-<?php   
+<?php
+// HEROKU DATABASE POSTGRES
+// $conn = pg_connect(getenv("DATABASE_URL")) or die('Conexão com banco de dados falhou');
+
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'], '/');
+
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$conn) { // Verificando a conexão
     $servername = "mysql";
     $username = "root";
     $password = "recode";
@@ -7,8 +22,8 @@
     // Criando conexão
     $conn = mysqli_connect($servername, $username, $password, $database);
 
-    // Verificando a conexão
-    if(!$conn) {
+    if (!$conn) { // Verificando a conexão
         die("A conexão com o Banco de dados falhou: " . mysqli_connect_error());
     }
+}
 ?>
